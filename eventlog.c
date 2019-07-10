cfa693849ab47b0e0ce93f3d05cd9ad73883833a
Gilles Chehade <gilles@poolp.org>
2019-07-10 19:38:35 +0200

    work in progress, introduce kv display to ease injection in elk parsing assumes perfectly crafted lines, i'll make that robust later

--- eventlog.c
+++ eventlog.c
@@ -21,12 +21,179 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
 
 #define	TIMEFRAME	86400
 
 static time_t curr_timeframe;
 
 extern char *__progname;
+extern int optind;
+extern char *optarg;
+
+static void
+print_raw(FILE *fp, char *line, size_t linelen)
+{
+	fwrite(line, linelen, 1, fp);
+}
+
+static void
+print_kv(FILE *fp, char *line, size_t linelen)
+{
+	char *save = line;
+	char *timestamp;
+	char *subsystem;
+	char *event;
+	char *session;
+
+	if (strncmp(line, "report|1|", 9) != 0)
+		return;
+	line[strcspn(line, "\n")] = '\0';
+
+	
+	line += 9;
+	linelen -= 9;
+
+	timestamp = line;
+	line[strcspn(line, "|")] = '\0';
+	line += strlen(line) + 1;
+
+	subsystem = line;
+	line[strcspn(line, "|")] = '\0';
+	line += strlen(line) + 1;
+
+	event = line;
+	line[strcspn(line, "|")] = '\0';
+	line += strlen(line) + 1;
+
+	session = line;
+	line[strcspn(line, "|")] = '\0';
+	line += strlen(line) + 1;
+
+	fprintf(fp, "timestamp=%s ", timestamp);
+	fprintf(fp, "subsystem=%s ", subsystem);
+	fprintf(fp, "event=%s ", event);
+	fprintf(fp, "session=%s ", session);
+
+	if (strcmp(event, "link-connect") == 0)
+		fprintf(fp, "line: %s", line);
+	else if (strcmp(event, "link-identify") == 0)
+		fprintf(fp, "helo-name=%s", line);
+	else if (strcmp(event, "link-tls") == 0)
+		fprintf(fp, "tls=%s", line);
+	else if (strcmp(event, "link-disconnect") == 0)
+		/* no params */;
+
+	else if (strcmp(event, "tx-begin") == 0) {
+		fprintf(fp, "msgid=%s", line);
+	}
+	else if (strcmp(event, "tx-mail") == 0 ||
+	    strcmp(event, "tx-rcpt") == 0) {
+		char *msgid;
+		char *address;
+		char *status;
+
+		msgid = line;
+		line[strcspn(line, "|")] = '\0';
+		line += strlen(line) + 1;
+
+		address = line;
+		line[strcspn(line, "|")] = '\0';
+		line += strlen(line) + 1;
+
+		status = line;
+
+		fprintf(fp, "msgid=%s address=%s status=%s",
+		    msgid, address, status);
+	}
+
+	else if (strcmp(event, "tx-envelope") == 0) {
+		char *msgid;
+		char *evpid;
+
+		msgid = line;
+		line[strcspn(line, "|")] = '\0';
+		line += strlen(line) + 1;
+
+		evpid = line;
+
+		fprintf(fp, "msgid=%s evpid=%s", msgid, evpid);
+	}
+	else if (strcmp(event, "tx-data") == 0) {
+		char *msgid;
+		char *status;
+
+		msgid = line;
+		line[strcspn(line, "|")] = '\0';
+		line += strlen(line) + 1;
+
+		status = line;
+
+		fprintf(fp, "msgid=%s status=%s", msgid, status);
+	}
+	else if (strcmp(event, "tx-commit") == 0) {
+		char *msgid;
+		char *size;
+
+		msgid = line;
+		line[strcspn(line, "|")] = '\0';
+		line += strlen(line) + 1;
+
+		size = line;
+
+		fprintf(fp, "msgid=%s size=%s", msgid, size);
+	}
+	else if (strcmp(event, "tx-rollback") == 0) {
+		char *msgid;
+
+		msgid = line;
+
+		fprintf(fp, "msgid=%s", msgid);
+	}
+
+
+	else if (strcmp(event, "protocol-client") == 0 ||
+	    strcmp(event, "protocol-server") == 0) {
+		fprintf(fp, "line=\"");
+		while (*line) {
+			if (*line == '"')
+				fprintf(fp, "\\");
+			fprintf(fp, "%c", *line);
+			line++;
+		}
+		fprintf(fp, "\"");
+	}
+
+	else if (strcmp(event, "filter-response") == 0) {
+		char *phase;
+		char *response;
+		char *param = NULL;
+
+		phase = line;
+		line[strcspn(line, "|")] = '\0';
+		line += strlen(line) + 1;
+
+		response = line;
+		line[strcspn(line, "|")] = '\0';
+		line += strlen(line) + 1;
+
+		fprintf(fp, "phase=%s response=%s", phase, response);
+
+		if (line < save + linelen) {
+			param = line;
+			fprintf(fp, "param=%s", param);
+		}
+	}
+	else if (strcmp(event, "timeout") == 0)
+		/* no params*/;
+
+	else {
+		fprintf(fp, "line: %s", line);
+	}
+
+	fprintf(fp, "\n");
+
+}
 
 int
 main(int argc, char *argv[])
@@ -39,8 +206,25 @@ main(int argc, char *argv[])
 	time_t ctime;
 	time_t timeframe;
 	char *ep;
+	int ch;
+	void (*print)(FILE *fp, char *line, size_t linelen) = print_raw;
 
-	if (argc != 2)
+	while ((ch = getopt(argc, argv, "t:")) != -1) {
+		switch (ch) {
+		case 't':
+			if (strcmp(optarg, "raw") == 0)
+				print = print_raw;
+			else if (strcmp(optarg, "kv") == 0)
+				print = print_kv;
+			else
+				errx(1, "unsupported type: %s", optarg);
+			break;
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc != 1)
 		errx(1, "usage: %s /path/to/directory", __progname);
 
 	printf("register|report|smtp-in|*\n");
@@ -62,16 +246,15 @@ main(int argc, char *argv[])
 		if (curr_timeframe == 0 || curr_timeframe != timeframe) {
 			if (fp)
 				fclose(fp);
-			else {
-				(void)snprintf(pathname, sizeof pathname, "%s/%lld",
-				    argv[1], timeframe);
-				if ((fp = fopen(pathname, "a+")) == NULL)
-					err(1, "fopen");
-				curr_timeframe = timeframe;
-			}
+			(void)snprintf(pathname, sizeof pathname, "%s/%lld",
+			    argv[0], timeframe);
+			if ((fp = fopen(pathname, "a+")) == NULL)
+				err(1, "fopen: %s", pathname);
+			curr_timeframe = timeframe;
 		}
 
-		fwrite(line, linelen, 1, fp);
+		print(fp, line, linelen);
+
 		fflush(fp);
 	}
 	free(line);
